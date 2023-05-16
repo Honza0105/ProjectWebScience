2276aa384b914eddc17befa88f74c902436c2da4
Honza0105 <honzajelinek@email.cz>
2023-05-16 15:32:29 +0200

    import stopwatch

--- functionGerman.py
+++ functionGerman.py
@@ -1,22 +1,24 @@
 import selenium
+import stopwatch
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common import by
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from stopwatch import Stopwatch
 
 
 def getAllSynonyms(word, driver):
     address = "https://www.dwds.de/wb/" + word
-    print(address)
+    # print(address)
 
-    print("Driver initialized")
+    # print("Driver initialized")
     driver.get(address)
     # Set up Selenium driver
 
     # Find the element using the XPath
     try:
-        element = driver.find_element(by.By.XPATH,"/html/body/main/div[1]/div/div[1]/div[2]/div/div[9]/div")
+        element = driver.find_element(by.By.XPATH, "/html/body/main/div[1]/div/div[1]/div[2]/div/div[9]/div")
     except selenium.common.exceptions.NoSuchElementException:
         return None
     # Get the content of the element
@@ -27,16 +29,15 @@ def getAllSynonyms(word, driver):
 
     return content
 
-    # Close the Selenium driver
-
 
 options = Options()
 options.add_argument("--headless")  # Run Chrome in headless mode
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-print(getAllSynonyms("Deutschland",driver))
-print(getAllSynonyms("Handy",driver))
-print(getAllSynonyms("Wort",driver))
+getAllSynonyms("Deutschland", driver)
+getAllSynonyms("Handy", driver)
+getAllSynonyms("Wort", driver)
+getAllSynonyms("Lied", driver)
 
 driver.quit()
