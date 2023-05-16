1ca828f5ee8c45b08407fb4d7526e741a57bd29d
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-16 22:39:25 +0200

    Update showcase.py
    changed it for edge browser

--- showcase.py
+++ showcase.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Set up Selenium driver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Edge(service = Service(verbose = True))
 driver.get("https://www.dwds.de/wb/Auto")
+
+# service = Service(verbose = True)
 
 # Find the element using the XPath
 element = driver.find_element_by_xpath("/html/body/main/div[1]/div/div[1]/div[2]/div/div[9]/div")
