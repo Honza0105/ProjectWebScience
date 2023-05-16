7fe19587c72090a300a299ed088f7502a66895fc
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-16 23:44:46 +0200

    Update showcase.py

--- showcase.py
+++ showcase.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.edge.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-from msedge.selenium_tools import EdgeOptions, Edge
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
-edge_options = EdgeOptions()
-edge_options.use_chromium = True
-edge_options.add_argument('headless')
-edge_options.add_argument('disable-gpu')
-driver = Edge(executable_path='C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe', options=edge_options)
 
+# edge_options = EdgeOptions()
+# edge_options.use_chromium = True
+# edge_options.add_argument('headless')
+# edge_options.add_argument('disable-gpu')
+# driver = Edge(executable_path='C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe', options=edge_options)
+options = EdgeOptions()
+options.add_argument("--headless=new")
+
+# C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe
 # Set up Selenium driver
 # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver = webdriver.Edge(service = Service(verbose = True))
+driver = webdriver.Edge(service = Service)
 driver.get("https://www.dwds.de/wb/Auto")
 
 # service = Service(verbose = True)
