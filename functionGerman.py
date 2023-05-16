8cf76aba6e200b8518456b325c8ab4eae9d7eb14
Honza0105 <honzajelinek@email.cz>
2023-05-16 11:17:07 +0200

    now it works headlessly

--- functionGerman.py
+++ functionGerman.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common import by
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def getAllSynonyms(word):
     address = "https://www.dwds.de/wb/" + word
     print(address)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = Options()
+    options.add_argument("--headless")  # Run Chrome in headless mode
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get(address)
     # Set up Selenium driver
 
