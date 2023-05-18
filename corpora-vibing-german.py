b5c5acf69ac82bd6892b9f061b5eb82287ee0f64
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-18 12:59:12 +0200

    Update corpora-vibing-german.py

--- corpora-vibing-german.py
+++ corpora-vibing-german.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 initial_corpora = "D:\\Mano\\_Uni\\NL\\NL year 2\\4.5 project\\corpora\\deu_news_2022_1M\\deu_news_2022_1M-words.txt"
 shortnened_corpora = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\Output\\output-deu_news_2022_1M-words.txt"
@@ -7,18 +8,25 @@ unique_words = set()
 removed_words = set()
 
 # Open the TSV and CSV files
-with open(initial_corpora, "rb") as file_in, open(shortnened_corpora, "w", newline="", encoding="utf-8") as file_out:
-    tsv_reader = csv.reader((line.decode("utf-8", errors="ignore") for line in file_in), delimiter="\t")  # Create a TSV reader, ignoring decoding errors
+with open(initial_corpora, "r", encoding="utf-8") as file_in, open(shortnened_corpora, "w", newline="", encoding="utf-8") as file_out:
+    initial_corpora_reader = csv.reader(file_in, delimiter="\t")  # Create a TSV reader
 
-    csv_writer = csv.writer(file_out)  # Create a CSV writer
-    for row in tsv_reader:
+    shortnened_corpora_writer = csv.writer(file_out)  # Create a CSV writer
+    for row in initial_corpora_reader:
         if len(row) >= 3:
             word = row[1].lower()  # Convert the word to lowercase
+            word = word.replace(".", "")
+            word = word.replace("’", "")
+            word = word.replace("”", "")
+            word = word.replace("‘", "")
+            word = word.replace(",", " ")
+            word = word.replace("  ", " ")
+            word = re.sub(r"\d", "", word)  # Remove numbers from the word
             if word in unique_words:
                 removed_words.add(word)  # Add the lowercase word to the set of duplicate words
             else:
                 unique_words.add(word)  # Add the lowercase word to the set of unique words
-                csv_writer.writerow([word])  # Write the lowercase word to the CSV file
+                shortnened_corpora_writer.writerow([word])  # Write the lowercase word to the CSV file
             
 # Write the duplicate words to a separate file
 removed_duplicates = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\Output\\removed_words-output-deu_news_2022_1M-words.txt"
