ac516e4ab61e380beebb67c5b224950a57898e6d
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-18 11:35:52 +0200

    Update corpora-vibing-dutch.py

--- corpora-vibing-dutch.py
+++ corpora-vibing-dutch.py
@@ -8,17 +8,17 @@ removed_words = set()
 
 # Open the TSV and CSV files
 with open(initial_corpora, "rb") as file_in, open(shortnened_corpora, "w", newline="", encoding="utf-8") as file_out:
-    tsv_reader = csv.reader((line.decode("utf-8", errors="ignore") for line in file_in), delimiter="\t")  # Create a TSV reader, ignoring decoding errors
+    initial_corpora_reader = csv.reader((line.decode("utf-8", errors="ignore") for line in file_in), delimiter="\t")  # Create a TSV reader, ignoring decoding errors
 
-    csv_writer = csv.writer(file_out)  # Create a CSV writer
-    for row in tsv_reader:
+    shortnened_corpora_writer = csv.writer(file_out)  # Create a CSV writer
+    for row in initial_corpora_reader:
         if len(row) >= 3:
             word = row[1].lower()  # Convert the word to lowercase
             if word in unique_words:
                 removed_words.add(word)  # Add the lowercase word to the set of duplicate words
             else:
                 unique_words.add(word)  # Add the lowercase word to the set of unique words
-                csv_writer.writerow([word])  # Write the lowercase word to the CSV file
+                shortnened_corpora_writer.writerow([word])  # Write the lowercase word to the CSV file
             
 # Write the duplicate words to a separate file
 removed_duplicates = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\Output\\removed_words-output-nld_news_2022_1M-words.txt"
