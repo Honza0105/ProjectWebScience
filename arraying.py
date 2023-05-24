7a4912e4f7c1079022b477e818242585d07951e0
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-24 16:54:42 +0200

    Update arraying.py

--- arraying.py
+++ arraying.py
@@ -9,14 +9,12 @@ new_array = []
 with open(initial_pickle, 'rb') as file:
     my_dictionary = pickle.load(file)
 
-# Convert the dictionary to an array
 for key, value in my_dictionary.items():
     if value is None:
         value = ""
     new_array.append({'key': key, 'value': value})
 
 with open(unpickled_nodes, 'w', encoding='utf-8') as file:
-# Save the dictionary as a human-readable file
     for entry in new_array:
         file.write(f"key: {entry['key']}\n")
         file.write(f"value: {entry['value']}\n")
