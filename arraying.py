c0dcefd5a497709f33310b91c85cbafc3827a543
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-24 15:43:04 +0200

    Update arraying.py

--- arraying.py
+++ arraying.py
@@ -1,5 +1,3 @@
-import csv
-import re
 import pickle
 
 initial_pickle = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\_pickles\\word_dutch.pkl"
@@ -11,20 +9,10 @@ new_array = []
 with open(initial_pickle, 'rb') as file:
     my_dictionary = pickle.load(file)
 
-new_array.append(my_dictionary)
+# Convert the dictionary to an array
+for key, value in my_dictionary.items():
+    new_array.append({'key': key, 'value': value})
 
-with open(unpickled_nodes, "w", encoding="utf-8") as file_duplicate:
-    for word in new_array:
-        unpickled_edges.write(word + "\n")
-
-my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-
-my_array = [{'key': key, 'value': value} for key, value in my_dict.items()]
-
-print(my_array)
-
-
-# print(new_array)
-
-# if thing is not None:
-#     print()
+# Print the new array
+for item in new_array:
+    print(item)
