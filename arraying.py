ce19227e63d16eea672290ee1eb5c0d39698a12e
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-24 13:18:38 +0200

    Create arraying.py

---
+++ arraying.py
@@ -0,0 +1,7 @@
+import csv
+import re
+
+initial_pickle = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\_pickles\\word_dutch.pkl"
+unpickled_nodes = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\Output\\unpickled_dutch_nodes.txt"
+unpickled_edges = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\Output\\unpickled_dutch_edges.txt"
+
