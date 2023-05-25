b41dec4174a6fd2e1f871a064ac93eefeb6a6939
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-25 11:44:13 +0200

    Update unpickling process.py

--- unpickling process.py
+++ unpickling process.py
@@ -1,17 +1,17 @@
 import pickle
 
-initial_pickle = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\_pickles\\word_dutch.pkl"
-unpickled_file = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\Output\\unpickled_dutch.py"
+initial_pickle = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\_pickles\\word_german.pkl"
+unpickled_file = "D:\\Mano\\Projektėliai\\GitHub\\ProjectWebScience\\Files for PWS\\Output\\unpickled_german.py"
 
 def unpickling_the_snake(dictionary, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
-        file.write("scraped_dutch = {\n")
+        file.write("scraped_german = {\n")
         for key in sorted(dictionary):
             value = dictionary[key]
             file.write(f"    {repr(key)}: {repr(value)},\n")
         file.write("}")
 
 with open(initial_pickle, 'rb') as file:
-    scraped_dutch = pickle.load(file)
+    scraped_german = pickle.load(file)
 
-unpickling_the_snake(scraped_dutch, unpickled_file)
+unpickling_the_snake(scraped_german, unpickled_file)
