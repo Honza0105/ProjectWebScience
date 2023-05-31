d313c05073074216d02bb31f9d3b0c2037448866
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2023-05-31 16:15:43 +0200

    reverted the changes to the last working iteration (i love github)

--- gephi_create.py
+++ gephi_create.py
@@ -4,34 +4,41 @@ import pickle
 def dictionary_to_CSV(original_file, nodes_target, edges_target):
     # Load data from pickle file
     with open(original_file, 'rb') as file:
+
+    # for key in word_synonyms.keys():
+    #     print(key)
+
+
+
         word_synonyms = pickle.load(file, encoding='latin1')
 
-    # Create a set to store unique labels
-    unique_labels = set()
+    # Create an array with unique words
+    # word_array = list(set(word_synonyms.keys()).union(*word_synonyms.values()))
+    keys = word_synonyms.keys()
+    value_sets = [set(v) for v in word_synonyms.values() if v is not None]
+    union = set(keys).union(*value_sets)
+    word_array = list(union)
+
+    print(word_array)
+
+
 
     # Create nodes CSV file
     with open(nodes_target, 'w', newline='', encoding='utf-8', errors='ignore') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Id', 'Label'])
-        for i, (word, targets) in enumerate(word_synonyms.items()):
-            if targets is not None:
-                # Lowercase the word and check if the lowercase label is unique
-                lowercase_word = word.lower()
-                if lowercase_word not in unique_labels:
-                    writer.writerow([i, lowercase_word])
-                    unique_labels.add(lowercase_word)
+        for i, word in enumerate(word_array):
+            writer.writerow([i, word])
 
     # Create edges CSV file
     with open(edges_target, 'w', newline='', encoding='utf-8', errors='ignore') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Source', 'Target', 'Type'])
         for source, targets in word_synonyms.items():
+            source_index = word_array.index(source)  # Get the index of the source word
             if targets is not None:
-                source_index = list(unique_labels).index(source.lower())  # Get the index of the source word
                 for target in targets:
-                    lowercase_target = target.lower()
-                    if lowercase_target in unique_labels:
-                        target_index = list(unique_labels).index(lowercase_target)  # Get the index of the target word
-                        writer.writerow([source_index, target_index, 'Directed'])
+                    target_index = word_array.index(target)  # Get the index of the target word
+                    writer.writerow([source_index, target_index, 'Directed'])
 
 dictionary_to_CSV("Files/repickled_german.pkl", "german_nodes.csv", "german_edges.csv")
